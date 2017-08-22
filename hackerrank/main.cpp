31857ff376381c1dd2d2a80addfc3b5c92a16586
Rakesh Shukla <rakeshshukla@Rakeshs-MacBook-Pro.local>
2017-08-22 10:29:47 +0530

    Initial Commit

---
+++ hackerrank/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hackerrank
+//
+//  Created by Rakesh Shukla on 22/08/17.
+//  Copyright © 2017 Rakesh Shukla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
